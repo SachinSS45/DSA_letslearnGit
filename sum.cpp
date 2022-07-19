734d0cf859c10e4d9d1c22f2a4cdf312811393cc
Sachin Shetkar <ssshetkar2001@gmail.com>
2022-07-20 05:05:55 +0530

    intitial commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+
+int main(){
+
+	int a , b;
+	std::cout << "Enter two numbers a and b : " << std::endl;
+	std::cin >> a >> b;
+
+	int c = a + b;
+	std::cout << "Sum is : " << c << std::endl;
+	return 0;
+}
